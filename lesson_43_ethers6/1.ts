f636f4d9ac8d3885f183ea45923360ce1bc50c49
bodrovis <bodrovis@protonmail.com>
2023-02-11 21:17:49 +0200

    lesson 43

---
+++ lesson_43_ethers6/1.ts
@@ -0,0 +1,41 @@
+import { ethers, Typed } from "ethers";
+
+//v5
+// import { providers } from "ethers"
+// const { InfuraProvider } = providers
+
+import { InfuraProvider } from "ethers";
+// import { AbiCoder } from "ethers";
+
+// const coder = AbiCoder.defaultAbiCoder()
+// console.log(coder.encode(
+//   ['string'], ['test']
+// ));
+
+const hex = ethers.toQuantity(BigInt(10));
+console.log(hex);
+
+const encoded = ethers.encodeBytes32String("test");
+console.log(encoded);
+console.log(ethers.decodeBytes32String(encoded));
+
+//v5
+//const hex = ethers.utils.hexValue(value)
+// ethers.utils.formatBytes32String
+// ethers.utils.parseBytes32String
+
+
+const abi = [
+  "function foo(address bar)",
+  "function foo(uint160 bar)",
+]
+
+const contract = new ethers.Contract("0x...", abi, provider);
+//contract.foo("0x000")
+contract["foo(address)"]("0x0000") // OK, v5
+contract["foo(address addr)"]("0x0000") // failed! v5
+
+contract["foo(address)"]("0x0000") // OK, v6
+contract["foo(address addr)"]("0x0000") // OK, v6
+contract.foo(Typed.address("0x000")) // v6
+contract.foo(Typed.uint160(100n));
